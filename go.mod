e581edb724163e2d57f2c98f822a0009ef82dfca
Matthew Mueller <mattmuelle@gmail.com>
2025-03-22 17:36:21 -0500

    add pgxstore support for a postgres session-based store

--- go.mod
+++ go.mod
@@ -3,23 +3,27 @@ module github.com/matthewmueller/sesh
 go 1.23.0
 
 require (
+	github.com/jackc/pgx/v5 v5.7.3
 	github.com/matryer/is v1.4.1
 	github.com/matthewmueller/diff v0.0.3
 	github.com/matthewmueller/httpbuf v0.0.2
 	github.com/mattn/go-sqlite3 v1.14.23
-	golang.org/x/sync v0.8.0
+	golang.org/x/sync v0.10.0
 )
 
 require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/hexops/valast v1.4.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/lithammer/dedent v1.1.0 // indirect
 	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/shurcooL/go-goon v0.0.0-20170922171312-37c2f522c041 // indirect
-	golang.org/x/mod v0.5.1 // indirect
-	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
-	golang.org/x/tools v0.1.8-0.20211102182255-bb4add04ddef // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	mvdan.cc/gofumpt v0.2.0 // indirect
 )
---
+++ pgxstore/pgxstore.go
@@ -0,0 +1,90 @@
+package pgxstore
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/matthewmueller/sesh"
+)
+
+// Schema for the session table
+const schema = `
+	CREATE TABLE IF NOT EXISTS %[1]s (
+		id TEXT PRIMARY KEY,
+		data BYTEA NOT NULL,
+		expiry BIGINT NOT NULL
+	);
+	CREATE INDEX IF NOT EXISTS %[1]s_expiry_idx ON %[1]s(expiry);
+`
+
+func New(db *pgxpool.Pool) *Store {
+	return &Store{db, "sessions", time.Now}
+}
+
+type Store struct {
+	db    *pgxpool.Pool
+	Table string
+
+	// Used for testing
+	Now func() time.Time
+}
+
+var _ sesh.Store = (*Store)(nil)
+
+func (s *Store) Migrate(ctx context.Context) error {
+	_, err := s.db.Exec(ctx, fmt.Sprintf(schema, s.Table))
+	return err
+}
+
+// Find returns the data for a session id from the store. If the session is not
+// found, expired or tampered, the data will be nil and the time will be zero,
+// but there will be no error.
+func (s *Store) Find(ctx context.Context, id string) (data []byte, expiry time.Time, err error) {
+	const query = `SELECT data, expiry FROM %[1]s WHERE id = $1`
+	row := s.db.QueryRow(ctx, fmt.Sprintf(query, s.Table), id)
+	var unixTimeSec int64
+	if err := row.Scan(&data, &unixTimeSec); err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			return nil, time.Time{}, err
+		}
+		// No session found
+		return nil, time.Time{}, nil
+	}
+	// Check if the session has expired
+	expiry = time.Unix(unixTimeSec, 0)
+	if expiry.Before(s.Now()) {
+		return nil, time.Time{}, nil
+	}
+	return data, expiry, nil
+}
+
+func (s *Store) Delete(ctx context.Context, id string) error {
+	const sql = `DELETE FROM %[1]s WHERE id = $1`
+	_, err := s.db.Exec(ctx, fmt.Sprintf(sql, s.Table), id)
+	return err
+}
+
+func (s *Store) Upsert(ctx context.Context, id string, data []byte, expiry time.Time) error {
+	const sql = `INSERT INTO %[1]s (id, data, expiry) VALUES ($1, $2, $3) ON CONFLICT(id) DO UPDATE SET data = $2, expiry = $3`
+	unixTimeSec := expiry.Unix()
+	_, err := s.db.Exec(ctx, fmt.Sprintf(sql, s.Table), id, data, unixTimeSec)
+	return err
+}
+
+// Cleanup removes expired sessions from the store.
+func (s *Store) Cleanup(ctx context.Context) error {
+	const sql = `DELETE FROM %[1]s WHERE expiry < $1`
+	_, err := s.db.Exec(ctx, fmt.Sprintf(sql, s.Table), s.Now().Unix())
+	return err
+}
+
+// Reset removes all sessions from the store.
+func (s *Store) Reset(ctx context.Context) error {
+	const sql = `DELETE FROM %[1]s`
+	_, err := s.db.Exec(ctx, fmt.Sprintf(sql, s.Table))
+	return err
+}
